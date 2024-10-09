395f3ea7d8901da4ca1b1556fd311afbb5af4d77
MH Code <mazheriftikhar1@gmail.com>
2024-10-09 17:36:41 +0500

    8 oct changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import OverviewPage from './pages/OverviewPage'
 import ProductsPage from './pages/ProductsPage'
 import Sidebar from './componets/Sidebar';
 import UsersPage from './pages/UsersPage';
+import SalesPage from './pages/SalesPage';
+import OrdersPage from './pages/OrdersPage';
 
 
 
@@ -23,6 +25,11 @@ const App = () => {
         <Route path='/' element={<OverviewPage />} />
         <Route path='/products' element={<ProductsPage />} />
         <Route path='/users' element={<UsersPage />} />
+        <Route path='/sales' element={<SalesPage />} />
+        <Route path='/orders' element={<OrdersPage />} />
+        {/* <Route path='/analytics' element={<AnalyticsPage />} />
+				<Route path='/settings' element={<SettingsPage />} /> */}
+      
       </Routes>
     </div>
   )
