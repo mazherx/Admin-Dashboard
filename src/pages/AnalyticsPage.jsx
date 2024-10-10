43123d907f7da57e909e4fb74830d7c24e8367b0
MH Code <mazheriftikhar1@gmail.com>
2024-10-10 13:32:16 +0500

    10-oct changes

---
+++ src/pages/AnalyticsPage.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Header from '../componets/Common/Header'
+import OverviewCards from '../componets/analytics/OverviewCards';
+import RevenueChart from '../componets/analytics/RevenueChart';
+import ChannelPerformance from '../componets/analytics/ChannelPerformance';
+import ProductPerformance from '../componets/analytics/ProductPerformance';
+import UserRetention from '../componets/analytics/UserRetention';
+import CustomerSegmentation from '../componets/analytics/CustomerSegmentation';
+
+
+
+
+const AnalyticsPage = () => {
+	return (
+		<div className='flex-1 overflow-auto relative z-10 bg-gray-900'>
+			<Header title={"Analytics Dashboard"} />
+
+			<main className='max-w-7xl mx-auto py-6 px-4 lg:px-8'>
+                <OverviewCards />
+                <RevenueChart />
+			</main>
+
+            <div className='grid grid-cols-1 lg:grid-cols-2 gap-5 mb-8 px-8'>
+					<ChannelPerformance />
+					<ProductPerformance />
+					<UserRetention />
+					<CustomerSegmentation />
+			</div>
+		</div>
+	);
+};
+export default AnalyticsPage;
---
+++ src/pages/SettingsPage.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Profile from '../componets/settings/Profile'
+import DangerZone from '../componets/settings/DangerZone'
+import Header from '../componets/Common/Header'
+import Notifications from '../componets/settings/Notifications'
+import Security from '../componets/settings/Security'
+import ConnectedAccounts from '../componets/settings/ConnectedAccounts'
+
+
+const SettingsPage = () => {
+    return (
+		<div className='flex-1 overflow-auto relative z-10 bg-gray-900'>
+			<Header title='Settings' />
+			<main className='max-w-4xl mx-auto py-6 px-4 lg:px-8'>
+                <Profile />
+                <Notifications />
+                <Security />
+                <ConnectedAccounts />
+                <DangerZone />
+            </main>
+    </div>
+  )
+}
+
+export default SettingsPage
